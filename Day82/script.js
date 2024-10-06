d2a2aaaa317090786aff9726884d16782a87d0b2
Debdhwoni <dhwoni77@gmail.com>
2024-10-06 15:22:14 +0530

    first commit

---
+++ Day82/script.js
@@ -0,0 +1,35 @@
+// async function sleep(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve(45)
+//         }, 2000);
+//     })
+// }
+// (async function main(){
+//     a = await sleep()
+//     b = await sleep()
+// })()
+// let [x,y,...z] = [[1,3,4,5,],[1,2,3,4],[12,455,666],74,44,4]
+// console.log(...x,...y,...z);
+
+// pull out 
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// }
+// console.log(obj.b);
+// let {a,b,c} = obj
+// console.log(b);
+// let arr = [1,2,3,4,5,6,7]
+// console.log(arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]+arr[6]);
+// console.log(sum(arr[0],arr[1]));
+// let arr = [1, 4, 6]
+// console.log(sum(arr[0], arr[1], arr[2]))
+// console.log(sum(...arr))
+
+
+
+
+
+
